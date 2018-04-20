19a6cf1bf892a7bf3373631eab987370a81ffe2f
Clare Pickering <clarelpickering@hotmail.com>
2018-04-20 17:18:39 +0100

    add css styling elements

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
+import "./styles/Stage.css";
+import Character from "./Character";
+import "./styles/weather.css";
 
 export class Stage extends Component {
   render() {
     console.log(this.props.docs);
     const { docs } = this.props;
     return (
-      <div>
-        <img src={docs[0].location.url} alt="location" />
+      <div
+        className="stage"
+        style={{ "background-image": `url(${docs[0].location.url})` }}
+      >
+        <Character className="character">
+          <img
+            className="characterImg"
+            src={docs[0].character.url}
+            alt={docs[0].character.name}
+          />
+        </Character>
+        <div className={docs[0].weather} />
       </div>
     );
   }
