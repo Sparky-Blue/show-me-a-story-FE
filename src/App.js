aa066f726baba99740e0bb813000d87f09feaaf6
Clare Pickering <clarelpickering@hotmail.com>
2018-04-20 12:28:56 +0100

    update app component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import AudioControl from "./components/AudioControl";
+import Message from "./components/Message";
+
+import "./App.css";
 
 class App extends Component {
+  state = {
+    message: "Passive",
+    BOT: "set_the_scene",
+    ACCESS_ID: "",
+    SECRET_KEY: ""
+  };
+
+  handleInputChange = event => {
+    const { target } = event;
+    const { value, name } = target;
+    this.setState({ [name]: value });
+  };
+
+  changeMessageTo = message => {
+    this.setState({ message });
+  };
+
   render() {
+    const { message, ACCESS_ID, SECRET_KEY, BOT } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="audio-control">
+        <AudioControl
+          accessId={ACCESS_ID}
+          secretKey={SECRET_KEY}
+          bot={BOT}
+          changeMessageTo={this.changeMessageTo}
+        />
+        <Message message={message} />
+
+        <p>
+          <input
+            type="password"
+            id="ACCESS_ID"
+            name="ACCESS_ID"
+            placeholder="ACCESS ID"
+            onChange={this.handleInputChange}
+            value={ACCESS_ID}
+          />
+        </p>
+        <p>
+          <input
+            type="password"
+            id="SECRET_KEY"
+            name="SECRET_KEY"
+            placeholder="SECRET KEY"
+            onChange={this.handleInputChange}
+            value={SECRET_KEY}
+          />
+        </p>
+        <p>
+          <input
+            type="text"
+            id="BOT"
+            name="BOT"
+            placeholder="BOT"
+            onChange={this.handleInputChange}
+            value={BOT}
+          />
         </p>
       </div>
     );
