83591b3f8955378764d98809899f4130ee8c3eeb
tehbri <brian.mcstay@gmail.com>
2018-04-20 14:57:09 +0100

    cleanup app and state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
-
 import AudioControl from "./components/AudioControl";
 import Message from "./components/Message";
-
+import { db } from "./firebase";
 import "./App.css";
+import { Stage } from "./components/Stage";
 
 class App extends Component {
   state = {
     message: "Passive",
-    BOT: "set_the_scene",
-    ACCESS_ID: "",
-    SECRET_KEY: ""
+    stageReady: false,
+    docs: []
   };
 
   handleInputChange = event => {
@@ -24,47 +23,11 @@ class App extends Component {
   };
 
   render() {
-    const { message, ACCESS_ID, SECRET_KEY, BOT } = this.state;
+    const { message, stageReady, docs } = this.state;
     return (
       <div className="audio-control">
-        <AudioControl
-          accessId={ACCESS_ID}
-          secretKey={SECRET_KEY}
-          bot={BOT}
-          changeMessageTo={this.changeMessageTo}
-        />
+        <AudioControl bot={this.BOT} changeMessageTo={this.changeMessageTo} />
         <Message message={message} />
-
-        <p>
-          <input
-            type="password"
-            id="ACCESS_ID"
-            name="ACCESS_ID"
-            placeholder="ACCESS ID"
-            onChange={this.handleInputChange}
-            value={ACCESS_ID}
-          />
-        </p>
-        <p>
-          <input
-            type="password"
-            id="SECRET_KEY"
-            name="SECRET_KEY"
-            placeholder="SECRET KEY"
-            onChange={this.handleInputChange}
-            value={SECRET_KEY}
-          />
-        </p>
-        <p>
-          <input
-            type="text"
-            id="BOT"
-            name="BOT"
-            placeholder="BOT"
-            onChange={this.handleInputChange}
-            value={BOT}
-          />
-        </p>
       </div>
     );
   }
--- src/components/AudioControl.js
+++ src/components/AudioControl.js
@@ -1,69 +1,75 @@
-import React from 'react';
-import AWS from 'aws-sdk';
+import React from "react";
+import AWS from "aws-sdk";
 
-import Visualizer from './Visualizer';
-import Waveform from '../utils/renderer';
+import Visualizer from "./Visualizer";
+import Waveform from "../utils/renderer";
 
-import LexAudio from '../utils/lexAudio';
-import lex from '../lex.png';
+import LexAudio from "../utils/lexAudio";
+import lex from "../lex.png";
 
 class AudioControl extends React.Component {
+  setWaveform = node => {
+    if (node) {
+      this.waveform = Waveform(node);
+    }
+  };
 
-    setWaveform = (node) => {
-        if (node) {
-            this.waveform = Waveform(node);
+  handleAudioControlClick = e => {
+    const bot = "set_the_scene";
+    const { changeMessageTo } = this.props;
+    const that = this;
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: "eu-west-1:3406189f-e003-43c7-a93b-95fa95a1b7b8"
+    });
+    AWS.config.region = "eu-west-1";
+
+    const config = {
+      lexConfig: {
+        botName: bot
+      }
+    };
+
+    this.conversation = new LexAudio.conversation(
+      config,
+      function(state) {
+        changeMessageTo(state + "...");
+        if (state === "Listening") {
+          that.waveform.prepCanvas();
         }
-    }
-
-    handleAudioControlClick = (e) => {
-        const {bot, accessId, secretKey, changeMessageTo } = this.props;
-        const that = this;
-        AWS.config.credentials = new AWS.Credentials(accessId, secretKey, null);
-        AWS.config.region = 'eu-west-1';
-
-        const config = {
-            lexConfig: {
-                botName: bot 
-            }
-        };
-
-        this.conversation = new LexAudio.conversation(config, function (state) {
-            changeMessageTo(state + '...');
-            if (state === 'Listening') {
-                that
-                    .waveform
-                    .prepCanvas();
-            }
-            if (state === 'Sending') {
-                that
-                    .waveform
-                    .clearCanvas();
-            }
-        }, function (data) {
-            console.log('Transcript: ', data.inputTranscript, ", Response: ", data.message);
-        }, function (error) {
-            changeMessageTo(error);
-        }, function (timeDomain, bufferLength) {
-            that
-                .waveform
-                .visualizeAudioBuffer(timeDomain, bufferLength);
-        });
-        this
-            .conversation
-            .advanceConversation();
-    }
-
-    render() {
-        return (
-            <p
-                id="audio-control"
-                onClick={this.handleAudioControlClick}
-                className="white-circle">
-                <img src={lex} alt="lex"/>
-                <Visualizer setWaveform={this.setWaveform} />
-            </p>
+        if (state === "Sending") {
+          that.waveform.clearCanvas();
+        }
+      },
+      function(data) {
+        console.log(
+          "Transcript: ",
+          data.inputTranscript,
+          ", Response: ",
+          data.message
         );
-    }
+      },
+      function(error) {
+        changeMessageTo(error);
+      },
+      function(timeDomain, bufferLength) {
+        that.waveform.visualizeAudioBuffer(timeDomain, bufferLength);
+      }
+    );
+    this.conversation.advanceConversation();
+  };
+
+  render() {
+    return (
+      <p
+        id="audio-control"
+        onClick={this.handleAudioControlClick}
+        className="white-circle"
+      >
+        <img src={lex} alt="lex" />
+        <Visualizer setWaveform={this.setWaveform} />
+      </p>
+    );
+  }
 }
 
 export default AudioControl;
