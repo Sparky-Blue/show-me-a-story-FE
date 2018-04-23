3a754718722e93354c59f8bab4a9762861d76899
jbrown136 <j.brown8@live.co.uk>
2018-04-23 17:19:48 +0100

    added sun weather animation

---
+++ src/components/Sun.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ".styles/Sun.css";
+
+class Sun extends Component {
+  render() {
+    return (
+      <div id="container">
+        ​<div class="theSun" />
+        <div class="cloud c1" />
+        <div class="cloud c2" />
+      </div>
+    );
+  }
+}
+
+export default Sun;
