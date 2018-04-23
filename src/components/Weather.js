78b1e87ad0cf9083c09e9a79d23b71db7ec124f9
Clare Pickering <clarelpickering@hotmail.com>
2018-04-23 09:10:47 +0100

    add weather changes componenent and css file

---
+++ src/components/Weather.js
@@ -0,0 +1,9 @@
+import React from "react";
+import posed from "react-pose";
+import { easing, tween } from "popmotion";
+
+const poseProps = {};
+
+const Weather = posed.div(poseProps);
+
+export default Weather;
