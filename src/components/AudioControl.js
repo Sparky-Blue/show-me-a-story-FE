764a5e1c303fff8e0cc9d03b3a921af33af98c37
Anthony Medina <antmedina91@gmail.com>
2018-04-26 21:05:25 +0100

    style button

--- src/components/AudioControl.js
+++ src/components/AudioControl.js
@@ -80,13 +80,13 @@ class AudioControl extends React.Component {
           playing={this.state.promptPlayed}
           onEnd={this.handleAudioControlClick}
         />
-        <button
+        <div
           id="audio-control"
           onClick={this.togglePromptPLayed}
-          className="white-circle"
+          className="button"
         >
-          {buttonMessage}
-        </button>
+          <h1>{buttonMessage}</h1>
+        </div>
         <Visualizer setWaveform={this.setWaveform} />
       </Fragment>
     );
