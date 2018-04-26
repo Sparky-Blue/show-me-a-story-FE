eb98190eff6fc1270178bac0f14ee1e1a03bd202
Clare Pickering <clarelpickering@hotmail.com>
2018-04-26 14:52:43 +0100

    update storm

--- src/components/Storm.js
+++ src/components/Storm.js
@@ -1,14 +1,13 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
 import "./styles/Storm.css";
-//import Rain from "./Rain.js";
-import Raindrop from "./Raindrop";
+import Rain from "./Rain.js";
 
 class Storm extends Component {
   state = {
     lightning: false
   };
   showLightning = () => {
-    const time = Math.random() * 55000 + 5000;
+    const time = Math.random() * 3000 + 3000;
     setTimeout(() => {
       this.setState({ lightning: true });
       setTimeout(() => {
@@ -22,20 +21,8 @@ class Storm extends Component {
       this.showLightning();
     }
     return (
-      <div className="container">
-        {/* <img
-          src="http://www.clker.com/cliparts/U/p/c/a/k/x/dark-cloud.svg"
-          alt=""
-          id="cloud1"
-        />
-        <img
-          src="http://www.clker.com/cliparts/U/p/c/a/k/x/dark-cloud.svg"
-          alt=""
-          id="cloud2"
-        /> */}
-        <div id="rain">
-          <Rain id="raindrops" />
-        </div>
+      <Fragment>
+        <Rain />
         {this.state.lightning && (
           <img
             src="http://www.pngall.com/wp-content/uploads/2017/01/Lightning-High-Quality-PNG.png"
@@ -49,40 +36,8 @@ class Storm extends Component {
             }}
           />
         )}
-      </div>
+      </Fragment>
     );
-  }
-}
-
-class Rain extends Component {
-  state = { entering: true };
-  componentDidMount() {
-    setTimeout(this.toggle, 1000);
-  }
-
-  toggle = () => this.setState({ entering: !this.state.entering });
-
-  render() {
-    const rain = new Array(180).fill();
-    return rain.map((raindrop, index) => {
-      return (
-        <Raindrop key={index} pose={this.state.entering ? "enter" : "exit"}>
-          <i className="fas fa-tint" />
-          {/* {index % 2 ? (
-            <img
-              src="http://pngimg.com/uploads/cat/cat_PNG103.png"
-              alt=""
-              style={{ height: "20vh" }}
-            />
-          ) : (
-            <img
-              src="http://www.mseye-design.co.uk/wordpress/wp-content/uploads/2013/06/jumpingdog-e1370956242603.png"
-              alt=""
-            />
-          )} */}
-        </Raindrop>
-      );
-    });
   }
 }
 
